ce0d91b7d2d345d3202393525d99ba4aed363c50
Zachary Boyd <zachary@zacharyboyd.nyc>
2019-04-14 01:59:13 -0400

    inital

---
+++ README.md
@@ -0,0 +1,5 @@
+This project explores some of the features of [webtorrent](https://webtorrent.io/) a BitTorrent library for node.js and the browser.
+
+Run with `docker-compose up` then navigate to [http://localhost:3000](http://localhost:3000). You should see [Big Buck Bunny](https://peach.blender.org/) being streamed from its magnet URI.
+
+Please give it minute to start buffering.
---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const WebTorrent = require('webtorrent');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.set('content-type', 'text/html');
+    res.send(`
+        <html>
+            <head>
+                <style type="text/css">
+                    body {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        flex-direction: column;
+                    }
+                    video {
+                        max-width: 640px;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1><a href=""https://peach.blender.org/>Big Buck Bunny</a></h1>
+                <video autoplay="autoplay">
+                    <source src="/video" type="video/mp4" />
+                </video>
+            </body>
+        </html>
+    `);
+});
+
+app.get('/video', (req, res) => {
+    res.set('content-type', 'video/mp4');
+    res.set('content-disposition', 'inline');
+
+    const client = new WebTorrent();
+
+    const bigBuckBunnyURI = 'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big%20Buck%20Bunny&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F';
+
+
+    client.add(bigBuckBunnyURI, function (torrent) {
+        const file = torrent.files.filter((file) => file.name === 'Big Buck Bunny.mp4')[0];
+        
+        file.createReadStream().pipe(res);
+    });
+});
+
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
